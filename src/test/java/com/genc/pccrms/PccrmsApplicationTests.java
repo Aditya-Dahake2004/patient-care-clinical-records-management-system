6ed3786196935b7993d3feef8574253f5f62418e
Dahake <2503813@cognizant.com>
2026-05-26 22:20:02 +0530

    Initial commit

---
+++ src/test/java/com/genc/pccrms/PccrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genc.pccrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PccrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
